4a59e8d6cd0fd1c593b9e4eb396cb9c41a535efc
Arno_lemoil <arno.lemoil@gmail.com>
2025-02-12 16:46:13 +0100

    qui suis-je

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import StarryBackground from "./components/StarryBackground";
 
 export default function Home() {
 	return (
-		<>
+		<div className="flex flex-col items-center justify-center">
 			<div className="flex justify-center gap-32 items-center mt-20">
 				<div className="text-right">
 					<h1 className="font-[Tackerlen] text-9xl">Arno Lemoil</h1>
@@ -23,14 +23,35 @@ export default function Home() {
 					></Image>
 				</span>
 			</div>
-			<Image
-				alt="Arno_Lemoil"
-				src="/assets/wave.svg"
-				width={1900}
-				height={20}
-				className="wave mt-20"
-			></Image>
+			<div className="whoami mt-20 mx-64 items-center flex">
+				<div className="absolute left-0 w-full">
+					<Image
+						alt="Arno_Lemoil"
+						src="/assets/wave.svg"
+						width={1900}
+						height={20}
+						className="wave"
+					></Image>
+				</div>
+				<div className="relative">
+					<h2 className="text-5xl font-[Tackerlen] mb-10">À propos de moi</h2>
+					<p className="text-base font-[Questrial] pl-10">
+						Je suis un développeur web passionné par la création de sites web et
+						d&apos;applications web. J&apos;aime apprendre de nouvelles choses
+						et rencontrer de nouvelles personnes. Je suis un développeur web
+						passionné par la création de sites web et d&apos;applications web.
+						J&apos;aime apprendre de nouvelles choses et rencontrer de nouvelles
+						personnes. e suis un développeur web passionné par la création de
+						sites web et d&apos;applications web. J&apos;aime apprendre de
+						nouvelles choses et rencontrer de nouvelles personnes. Je suis un
+						développeur web passionné par la création de sites web et
+						d&apos;applications web. J&apos;aime apprendre de nouvelles choses
+						et rencontrer de nouvelles personnes.
+					</p>
+				</div>
+			</div>
+
 			<StarryBackground />
-		</>
+		</div>
 	);
 }
